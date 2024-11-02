2e6c2e4e863cd9da4b75515a51942fb4db1fa20b
Nithin <nithintejesh@gmail.com>
2024-11-02 13:28:55 +0530

    hide secret key

--- text_summarizer/settings.py
+++ text_summarizer/settings.py
@@ -21,10 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
+print(SECRET_KEY)
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # set to True when not in production
 
 # ALLOWED_HOSTS = ['easyextractivesummarizer.pythonanywhere.com']
 ALLOWED_HOSTS = ['*']
@@ -161,8 +168,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
